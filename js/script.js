5eeace3679ba514c04a6582757366d9fff7b80eb
juanmoncayo55 <147448954+juanmoncayo55@users.noreply.github.com>
2023-11-25 20:48:21 -0500

    Reparando Menu

--- js/script.js
+++ js/script.js
@@ -2,7 +2,8 @@
 (function(d){
 	const closeMenu = d.querySelector("#closeMenu"),
 		navigation = d.querySelector("#navigation"),
-		openMenu = d.querySelector("#openMenu")
+		openMenu = d.querySelector("#openMenu"),
+		mediaQuery = window.matchMedia('(min-width: 980px)')
 
 	openMenu.addEventListener('click', function(e){
 		e.preventDefault()
@@ -10,6 +11,9 @@
 		console.log("Listo para cerrar")
 		closeMenu.style.setProperty("display", "inline-block");
 		navigation.style.setProperty("left", "0px");
+		if (mediaQuery.matches) {
+		  closeMenu.style.setProperty("display", "none");
+		}
 	})
 
 	closeMenu.addEventListener("click", function(e){
@@ -17,5 +21,8 @@
 		navigation.style.setProperty("left", "-1000px");
 		closeMenu.style.setProperty("display", "none");
 	})
+
+
+
 
 })(document)
